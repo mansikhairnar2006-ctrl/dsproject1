a1aa9c3de545d910a013e1ef30893779fc0605f8
Mansi Sachin Khairnar <mansisachinkhairnar@Mansis-MacBook-Air.local>
2026-03-19 20:00:46 +0530

    first setup commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+name='dataScienceproject',
+version='0.0.1',
+author='mansi',
+author_email='mansikhairnar2006@gmail.com',
+packages=find_packages(),
+install_requires=['numpy','pandas']
+)
