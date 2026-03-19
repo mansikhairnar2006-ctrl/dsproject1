620a93fa5b74059839d378962749c1bf5658cafa
Mansi Sachin Khairnar <mansisachinkhairnar@Mansis-MacBook-Air.local>
2026-03-19 20:02:10 +0530

    first setup commit

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n',"")for req in requirements]
+
+    return requirements
+
+
+
 
 setup(
 name='dataScienceproject',
@@ -6,5 +18,5 @@ version='0.0.1',
 author='mansi',
 author_email='mansikhairnar2006@gmail.com',
 packages=find_packages(),
-install_requires=['numpy','pandas']
+install_requires=get_requirements('requirements.txt')
 )
